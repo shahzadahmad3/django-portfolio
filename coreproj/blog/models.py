15755673b0f45b309b6da926f0389e17b00d63f7
shahzadahmad3 <cs.shahzad1@gmail.com>
2025-09-08 11:38:09 +0500

    Day3- Added Author, Category models and practice ORM

--- coreproj/blog/models.py
+++ coreproj/blog/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
+
+    #Relationships
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    categories = models.ManyToManyField(Category, related_name='posts')
 
     def __str__(self):
         return self.title
